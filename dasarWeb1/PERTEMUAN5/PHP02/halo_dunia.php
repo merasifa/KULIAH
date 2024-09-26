fbc359c0b7cf3802836e196f00fe56ed0eccb1f7
merasifa <merasifa@gmail.com>
2024-09-26 15:51:51 +0700

    tugas

---
+++ dasarWeb1/PERTEMUAN5/PHP02/halo_dunia.php
@@ -0,0 +1,8 @@
+<?php
+
+$nama = @$_GET['mera'];
+$usia = @$_GET['19'];
+
+echo "Halo {$nama}! Apakah benar anda berusia {$usia} tahun?";
+
+?>
---
+++ dasarWeb1/PERTEMUAN5/PHP02/time.php
@@ -0,0 +1,11 @@
+<html>
+    <head>
+    </head>
+    <body>
+        <h3> Time </h3>
+        <?php
+        date_default_timezone_set("Asia/Jakarta"); // Perbaikan: menambahkan tanda titik koma
+        echo date("h:i:sa");
+        ?>
+    </body>
+</html>
