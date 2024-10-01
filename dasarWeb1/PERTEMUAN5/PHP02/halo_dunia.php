300fa70df4c2d1411b0f1e45551dc33e415fe0f5
merasifa <merasifa@gmail.com>
2024-10-01 12:25:49 +0700

    tugas

--- dasarWeb1/PERTEMUAN5/PHP02/halo_dunia.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-$nama = @$_GET['mera'];
-$usia = @$_GET['19'];
-
-echo "Halo {$nama}! Apakah benar anda berusia {$usia} tahun?";
-
-?>
--- dasarWeb1/PERTEMUAN5/PHP02/jquery-3.7.1.js
+++ dasarWeb1/PERTEMUAN5/PHP02/jquery-3.7.1.js
@@ -258,7 +258,7 @@ jQuery.extend = jQuery.fn.extend = function() {
 		i = 1,
 		length = arguments.length,
 		deep = false;
-
+          
 	// Handle a deep copy situation
 	if ( typeof target === "boolean" ) {
 		deep = target;
